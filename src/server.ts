e1ad3c3994e694d8dced889fb713a267fec836ca
Fabrício Santos <120188885+Fabricio-developer@users.noreply.github.com>
2023-08-26 20:09:21 -0300

    Feat: add mongoDB

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import mongoose from 'mongoose'
+import cors from 'cors';
+
+const routes = require('./routes/routes');
+
+// DB connection
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routing
+
+app.use('/api', routes)
+
+app.listen(3001, () => console.log("Server is running at http://localhost:3001"))
+
+
